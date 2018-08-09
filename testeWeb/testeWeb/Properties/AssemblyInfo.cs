73a9fcf3c3c6a257f4c55ddf86816351a855be21
Marjorie Almeida Barotti <marjorie-ab@hotmail.com>
2018-08-08 22:20:54 -0300

    first commit.

---
+++ testeWeb/testeWeb/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As Informações Gerais sobre um assembly são controladas por meio do 
+// conjunto de atributos a seguir. Altere esses valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("testeWeb")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("testeWeb")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Configurar o ComVisible como false torna os tipos desse assembly invisíveis 
+// para componentes COM.  Se for necessário acessar um tipo nesse assembly a partir do 
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// A GUID a seguir será referente à ID do typelib se este projeto for exposto ao COM
+[assembly: Guid("09d8a9c1-91e6-4c14-a329-0022e27e2a5e")]
+
+// As informações de versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+// É possível especificar todos os valores ou utilizar como padrão os Números de Revisão e da Versão 
+// usando o '*' como mostrado abaixo:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ testeWeb/testeWeb/WebForm1.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="testeWeb.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>NOTAS</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            NOTAS</div>
+        <p>
+            teste de um sistema CRUD de notas</p>
+        ID da nota:
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        <br />
+        Data de emissão da nota:
+        <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+        <br />
+        Valor da nota:
+        <asp:TextBox ID="TextBox3" runat="server" OnTextChanged="TextBox3_TextChanged"></asp:TextBox>
+        <br />
+    </form>
+</body>
+</html>
